09c5b3f1d7e7a2da7f81ba7b9e62d7abc840ba70
Joseph Hsu <jhsu.x1@gmail.com>
2012-06-12 20:04:34 -0400

    basic showing of peers

--- app/picker_controller.rb
+++ app/picker_controller.rb
@@ -2,14 +2,35 @@ class PickerController < UIViewController
   def viewDidAppear(animated)
     @picker = GKPeerPickerController.alloc.init
     @picker.delegate = self
-    @session = GKSession.alloc.initWithSessionID(nil, displayName: "hello#{rand(100)}", sessionMode: GKSessionModePeer)
+    @session = GKSession.alloc.initWithSessionID("connect", displayName: nil, sessionMode: GKSessionModePeer)
     @session.delegate = self
     @session.available = true
-    NSLog(@session.displayName)
     @picker.show
   end
 
-  def peerPickerControllerDidCancel(picker)
-    picker.delegate = nil
+  # GKPeerPickerController
+
+
+  def didSelectConnectionType(type)
   end
+
+  # GKPeerPickerControllerDelegate
+  # didSelectConnectionType:
+  # sessionForConnectionType:
+
+  # GKSesssion
+  # connectToPeer:withTimeout:
+  # cancelConnectToPeer:
+  # acceptConnectionFromPeer:error:
+  # denyConnectionFromPeer:
+
+  # def peerPickerControllerDidCancel(picker)
+  #   picker.delegate = nil
+  # end
+
+  # GKSessionDelegate
+  # didChangeState:
+  # didReceiveConnectionRequestFromPeer:
+  # connectionWithPeerFailed:withError:
+  # didFailWithError:
 end
