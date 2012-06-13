f1691bcccb645afa627731d258d5f4912bfae864
Joseph Hsu <jhsu.x1@gmail.com>
2012-06-12 22:37:14 -0400

    working connection

--- app/picker_controller.rb
+++ app/picker_controller.rb
@@ -1,40 +1,69 @@
 class PickerController < UIViewController
   def viewDidAppear(animated)
-    session = GKSession.alloc.initWithSessionID("connect", displayName: nil, sessionMode: GKSessionModePeer)
-    session.delegate = self
-    session.available = true
     @picker = GKPeerPickerController.alloc.init
     @picker.delegate = self
     @picker.show
-    alert("ZOMG")
   end
 
   # GKPeerPickerControllerDelegate
-  # didSelectConnectionType:
-  # sessionForConnectionType:
 
-  # GKSesssion
-  # connectToPeer:withTimeout:
-  # cancelConnectToPeer:
-  # acceptConnectionFromPeer:error:
-  # denyConnectionFromPeer:
-
-  # def peerPickerControllerDidCancel(picker)
-  #   picker.delegate = nil
-  # end
-
-  # GKSessionDelegate
-  # didChangeState:
-  # didReceiveConnectionRequestFromPeer:
-  # connectionWithPeerFailed:withError:
-  # didFailWithError:
-
-  def alert(msg)
-    dialog = UIAlertView.alloc.initWithTitle("GK", message: msg, delegate:nil, cancelButtonTitle: "OK", otherButtonTitles:nil)
-    dialog.show
+  def peerPickerController(picker, didSelectConnectionType: type)
+    alert("connectToPeer")
   end
 
-  def didReceiveConnectionRequestFromPeer(peerID)
-    alert("ZOMG")
+  def peerPickerController(picker, sessionForConnectionType: type)
+    alert("sessionForConnectionType")
+    session = GKSession.alloc.initWithSessionID("connect", displayName: nil, sessionMode: GKSessionModePeer)
+    session.delegate = self
+    session.available = true
+    session
+  end
+
+  def peerPickerController(picker, didConnectPeer: peer_id, toSession: session)
+    alert("didConnectPeer")
+    picker.dismiss
+  end
+
+  def peerPickerControllerDidCancel(picker)
+    alert("cancel")
+  end
+
+  # GKSessionDelegate
+  def session(session, peer: peer, didChangeState: state)
+    alert("state: #{state}")
+    case state
+    when GKPeerStateAvailable
+      alert("available")
+    when GKPeerStateUnavailable
+    when GKPeerStateConnected
+      alert(session.displayNameForPeer(peer))
+    when GKPeerStateDisconnected
+    when GKPeerStateConnecting
+      alert("connecting")
+    end
+  end
+
+  def session(session, didReceiveConnectionRequestFromPeer: peer_id)
+    alert("didReceiveConnectionRequestFromPeer")
+  end
+
+  def session(session, connectionWithPeerFailed:peerId, withError: error)
+    alert("didReceiveConnectionRequestFromPeer")
+  end
+
+  def connectionWithPeerFailed(peer_id, withError: error)
+    alert("didReceiveConnectionRequestFromPeer")
+  end
+
+  def session(session, didFailWithError: error)
+    alert("didFailWithError")
+  end
+
+  protected
+
+  def alert(msg)
+    # dialog = UIAlertView.alloc.initWithTitle("GK", message: msg, delegate:nil, cancelButtonTitle: "OK", otherButtonTitles:nil)
+    # dialog.show
+    NSLog(msg)
   end
 end
